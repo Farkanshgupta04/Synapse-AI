65b3380aeb58833fd0d12c3bdd49c8394130952a
Farkansh Gupta <farkanshgupta@gmail.com>
2026-05-21 19:08:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ const port = process.env.PORT || 3001;
 const MONGO_URL=process.env.MONGO_URL;
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'http://localhost:5173',
-  'http://localhost:5174',
+  'https://synapse-ai-ten-sable.vercel.app/',
 ].filter(Boolean);
 
 
